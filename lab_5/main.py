b870b39270ffa60ba5f8d40de3f2fd4f7e490bcc
mprostak <maciekprostak@gmail.com>
2021-12-30 17:51:02 +0100

    Refactor measurements

--- lab_5/main.py
+++ lab_5/main.py
@@ -18,69 +18,61 @@ test_labels_file = "dataset/t10k-labels.idx1-ubyte"
 test_labels = idx2numpy.convert_from_file(test_labels_file)
 
 
-def calculate(network: Network,
-              data,
-              matrix: list) -> int:
-    good_sum = 0
+def create_confusion_matrix(network: Network, data, classes) -> np.array:
+    confusion_matrix = np.zeros((len(classes), len(classes)))
+
     for _, number in enumerate(data):
         pixels, number_label = number
         output = network.feed_forward(pixels)
-        good_sum += number_label == output.argmax()
-
         result: int = int(number_label)
-        matrix[result][output.argmax()] += 1
-
-    return good_sum
-
-
-def make_confusion_matrix(matrix) -> dict:
-    confusion_matrix = {}
-    for number in range(10):
-        confusion_matrix[number] = {"tp": 0, "tn": 0, "fn": 0, "fp": 0}
-
-    for number in range(10):
-        confusion_matrix[number]["tp"] = matrix[number][number]
-        confusion_matrix[number]["tn"] = sum(
-            [row[i] for i, row in enumerate(matrix) if i != number]
-        )
-        confusion_matrix[number]["fp"] = sum(
-            [row[number] for i, row in enumerate(matrix) if i != number]
-        )
-        confusion_matrix[number]["fn"] = sum(
-            matrix[number][:number] + matrix[number][number + 1:]
-        )
+        confusion_matrix[result][output.argmax()] += 1
 
     return confusion_matrix
 
 
-def get_recall_fallout_precision_accuracy(
-                                          confusion_matrix: dict
-                                         ) -> Tuple[float, float, float, float]:
+def create_measurements(matrix: np.array) -> dict:
+    measurements = {}
+    for number in range(10):
+        measurements[number] = {"tp": 0, "tn": 0, "fn": 0, "fp": 0}
+
+    for number in range(10):
+        measurements[number]["tp"] = matrix[number][number]
+        measurements[number]["tn"] = sum(
+            [row[i] for i, row in enumerate(matrix) if i != number]
+        )
+        measurements[number]["fp"] = sum(
+            [row[number] for i, row in enumerate(matrix) if i != number]
+        )
+        # TODO
+        # measurements[number]["fn"] = sum(matrix[number][:number] + matrix[number][number + 1 :])
+
+    return measurements
+
+
+def get_measurements(
+    confusion_matrix: dict,
+) -> Tuple[float, float, float, float]:
     for key in confusion_matrix:
         try:
             recall = sum([confusion_matrix[key]["tp"]]) / (
-                sum([confusion_matrix[key]["tp"]]) +
-                sum([confusion_matrix[key]["fn"]])
+                sum([confusion_matrix[key]["tp"]]) + sum([confusion_matrix[key]["fn"]])
             )
         except ZeroDivisionError:
             recall = -1
         try:
             fallout = sum([confusion_matrix[key]["fp"]]) / (
-                sum([confusion_matrix[key]["fp"]]) +
-                sum([confusion_matrix[key]["tn"]])
+                sum([confusion_matrix[key]["fp"]]) + sum([confusion_matrix[key]["tn"]])
             )
         except ZeroDivisionError:
             fallout = -1
         try:
             precision = sum([confusion_matrix[key]["tp"]]) / (
-                sum([confusion_matrix[key]["tp"]]) +
-                sum([confusion_matrix[key]["fp"]])
+                sum([confusion_matrix[key]["tp"]]) + sum([confusion_matrix[key]["fp"]])
             )
         except ZeroDivisionError:
             precision = -1
         try:
-            accuracy = (sum([confusion_matrix[key]["tp"]]) +
-                        sum([confusion_matrix[key]["tn"]])) / (
+            accuracy = (sum([confusion_matrix[key]["tp"]]) + sum([confusion_matrix[key]["tn"]])) / (
                 sum([confusion_matrix[key]["tp"]])
                 + sum([confusion_matrix[key]["fn"]])
                 + sum([confusion_matrix[key]["tn"]])
@@ -92,6 +84,15 @@ def get_recall_fallout_precision_accuracy(
     return recall, fallout, precision, accuracy
 
 
+def prepare_data(images, labels) -> List[Tuple[np.array, int]]:
+    network_input_size = images[0].flatten().size
+
+    return [
+        ((image.flatten() / 255).reshape((network_input_size, 1)), int(label))
+        for image, label in zip(images, labels)
+    ]
+
+
 def main() -> None:
     # number of pixels in photo
     network_input_size = all_images[0].flatten().size
@@ -99,31 +100,21 @@ def main() -> None:
     # numbers in <0, 9>
     network_output_size = 10
 
-    network = Network([network_input_size, 10, 10, network_output_size],
-                      learning_rate=0.1)
+    network = Network([network_input_size, 10, 10, network_output_size], learning_rate=0.1)
 
-    train_data = [
-        ((image.flatten() / 255).reshape((network_input_size, 1)), int(label))
-        for image, label in zip(all_images, all_labels)
-    ]
+    train_data = prepare_data(all_images, all_labels)
     np.random.shuffle(train_data)
 
-    test_data = [
-        ((image.flatten() / 255).reshape((network_input_size, 1)), int(label))
-        for image, label in zip(test_images, test_labels)
-    ]
+    test_data = prepare_data(test_images, test_labels)
     np.random.shuffle(test_data)
 
     number_of_epochs = 20
     batch_size = 16
 
-    epochs_test_data = []
-    epochs_train_data = []
+    all_classes = [f"{x}" for x in range(10)]
 
-    train_matrix, test_matrix = [
-                                    [[0 for _ in range(10)] for _ in range(10)]
-                                    for _ in range(2)
-                                ]
+    parameters = ["recall", "fallout", "precision", "accuracy"]
+    all_measurements = {parameter: {"train": [], "test": []} for parameter in parameters}
 
     for epoch in range(number_of_epochs):
         epoch_start = perf_counter()
@@ -134,7 +125,7 @@ def main() -> None:
         for i in range(0, len(train_data) - batch_size, batch_size):
             mini_batch: List[Tuple[np.array, np.array]] = []
 
-            for _, number in enumerate(train_data[i: i + batch_size]):
+            for _, number in enumerate(train_data[i : i + batch_size]):
                 pixels, number_label = number
                 results: np.array = helpers.make_output(number_label)
                 mini_batch.append((pixels, results))
@@ -144,38 +135,22 @@ def main() -> None:
         epoch_end = perf_counter()
         print(f"Elapsed time: {epoch_end - epoch_start:.2f}")
 
-        test_ok = calculate(network, test_data, test_matrix)
-        train_ok = calculate(network, train_data, train_matrix)
+        test_confusion_matrix = create_confusion_matrix(network, test_data, all_classes)
+        train_confusion_matrix = create_confusion_matrix(network, train_data, all_classes)
 
-        test_confusion_matrix = make_confusion_matrix(test_matrix)
-        train_confusion_matrix = make_confusion_matrix(train_matrix)
+        test_measurement = create_measurements(test_confusion_matrix)
+        train_measurement = create_measurements(train_confusion_matrix)
 
-        test_paremeters = (
-            get_recall_fallout_precision_accuracy(test_confusion_matrix)
-        )
-        train_paremeters = (
-            get_recall_fallout_precision_accuracy(train_confusion_matrix)
-        )
+        test_parameters = get_measurements(test_measurement)
+        train_parameters = get_measurements(train_measurement)
 
-        parameters = ["recall   ", "fallout   ", "precision", "accuracy"]
-        print("PARAM\t\tTRAIN\tTEST")
         for index, name in enumerate(parameters):
-            print(name,
-                  round(train_paremeters[index], 2),
-                  round(test_paremeters[index], 2),
-                  sep="\t"
-                  )
+            all_measurements[name]["train"].append(train_parameters[index])
+            all_measurements[name]["test"].append(test_parameters[index])
 
-        # epochs_train_data.append((recall, fallout, precision, accuracy))
+        print(all_measurements)
 
-        epochs_train_data.append(train_ok / len(train_data))
-        epochs_test_data.append(test_ok / len(test_data))
-
-        msg = f"Train: {train_ok} / {len(train_data)} |"
-        msg += f" Test: {test_ok} / {len(test_data)}\n"
-        print(msg)
-
-    helpers.draw_network_epochs(epochs_train_data, epochs_test_data)
+    # helpers.draw_network_epochs()
 
 
 if __name__ == "__main__":
