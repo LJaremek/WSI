e44818eed662c079326f94c707b491a99780b8f6
Łukasz Jaremek <Jaremek.Lukasz@gmail.com>
2022-01-22 22:03:45 +0100

    Add metrics and documentation

--- lab_7/main.py
+++ lab_7/main.py
@@ -28,15 +28,59 @@ if __name__ == "__main__":
     bayes = NaiveBayes(train_data, label_index)
 
     # predict model
-    ok = 0
-    not_ok = 0
-    for index, row in test_data.iterrows():
-        drop_row = row.drop(label_index, 0)
-        if row[0] != bayes.check_row(drop_row):
-            print(row[0], bayes.check_row(drop_row))
-            not_ok += 1
-        else:
-            ok += 1
+    class_list = list("123")
 
-    print("not ok", not_ok)
-    print("ok", ok)
+    results = [[0 for _ in class_list] for _ in class_list]
+    for index, row in test_data.iterrows():
+        desired_result = row[label_index]
+        row = row.drop(label_index, 0)
+        obtained_result = bayes.check_row(row)
+
+        index_0 = class_list.index(desired_result)
+        index_1 = class_list.index(obtained_result)
+        results[index_0][index_1] += 1
+
+    # calculate metrics
+    metrics = {}
+    for y in class_list:
+        metrics[y] = {"tp": 0,
+                      "tn": 0,
+                      "fn": 0,
+                      "fp": 0}
+
+    # print results
+    print("\n\t", "\t".join(class_list))
+    for index, value in enumerate(class_list):
+        print(value, "\t", "\t".join(list(map(str, results[index]))))
+
+    # calculate
+    for index, y in enumerate(class_list):
+        metrics[y]["tp"] = results[index][index]
+        metrics[y]["tn"] = sum([row[i]
+                                for i, row in enumerate(results)
+                                if i != index])
+        metrics[y]["fp"] = sum([row[index]
+                                for i, row in enumerate(results)
+                                if i != index])
+        metrics[y]["fn"] = sum(results[index][:index] +
+                               results[index][index+1:])
+
+    # print Matrix
+    print("\nMatrix:")
+    for key in metrics:
+        print(key, metrics[key])
+
+    # Get and print additional parameters
+    tpr = sum([metrics[key]["tp"]]) / (sum([metrics[key]["tp"]]) +
+                                       sum([metrics[key]["fn"]]))
+    fpr = sum([metrics[key]["fp"]]) / (sum([metrics[key]["fp"]]) +
+                                       sum([metrics[key]["tn"]]))
+    ppv = sum([metrics[key]["tp"]]) / (sum([metrics[key]["tp"]]) +
+                                       sum([metrics[key]["fp"]]))
+    acc = ((sum([metrics[key]["tp"]]) + sum([metrics[key]["tn"]])) /
+           (sum([metrics[key]["tp"]]) + sum([metrics[key]["fn"]]) +
+            sum([metrics[key]["tn"]]) + sum([metrics[key]["fn"]])))
+    print("\nTPR:", tpr)
+    print("FPR:", fpr)
+    print("PPV:", ppv)
+    print("Acc:", acc)
